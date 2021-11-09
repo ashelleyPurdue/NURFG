1d9e356d5da057f4979256802b1bfdbb97efb1ce
Alex Shelley <alexthepeppershelley@gmail.com>
2021-11-08 20:08:24 -0500

    Add LambdaListener

---
+++ addons/GodotNUnitRunner/LambdaListener.cs
@@ -0,0 +1,24 @@
+using System;
+using NUnit.Framework.Interfaces;
+
+namespace GodotNUnitRunner
+{
+    public class LambdaListener : ITestListener
+    {
+        public Action<TestMessage> SendMessageCallback;
+        public void SendMessage(TestMessage message)
+            => SendMessageCallback?.Invoke(message);
+
+        public Action<ITestResult> TestFinishedCallback;
+        public void TestFinished(ITestResult result)
+            => TestFinishedCallback?.Invoke(result);
+
+        public Action<TestOutput> TestOutputCallback;
+        public void TestOutput(TestOutput output)
+            => TestOutputCallback?.Invoke(output);
+
+        public Action<ITest> TestStartedCallback;
+        public void TestStarted(ITest test)
+            => TestStartedCallback?.Invoke(test);
+    }
+}
