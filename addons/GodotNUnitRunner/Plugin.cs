bf7f00cedb01b4bbbe8619c5d54de240fb429ece
Alex Shelley <alexthepeppershelley@gmail.com>
2021-11-08 19:11:12 -0500

    Add a widget

---
+++ addons/GodotNUnitRunner/Plugin.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+namespace GodotNUnitRunner
+{
+    [Tool]
+    public class Plugin : EditorPlugin
+    {
+        private Control _dock;
+
+        public override void _EnterTree()
+        {
+            _dock = (Control)GD.Load<PackedScene>("addons/GodotNUnitRunner/EditorWidget/TestRunnerDock.tscn").Instance();
+            AddControlToDock(DockSlot.LeftUl, _dock);
+        }
+
+        public override void _ExitTree()
+        {
+            // Clean-up of the plugin goes here.
+            // Remove the dock.
+            RemoveControlFromDocks(_dock);
+
+            // Erase the control from the memory.
+            _dock?.Free();
+        }
+    }
+}
